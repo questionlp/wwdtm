43aa210703d442fb98189fe5e22b15e70447ab9e
Linh Pham <question+github@closedsrc.org>
2021-11-28 17:01:20 -0800

    Bump Sphinx from 4.2.0 to 4.3.1

--- wwdtm/__init__.py
+++ wwdtm/__init__.py
@@ -25,4 +25,4 @@ from wwdtm.scorekeeper import (Scorekeeper,
                                ScorekeeperAppearances,
                                ScorekeeperUtility)
 
-VERSION = "2.0.0-alpha.1"
+VERSION = "2.0.0-alpha.2"
