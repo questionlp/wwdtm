ec76e52d1276d744c4da2eb11e341a7520ce6949
Linh Pham <question+github@closedsrc.org>
2022-01-01 23:38:36 -0800

    Correct config.json.dist entry in MANIFEST.in

--- wwdtm/__init__.py
+++ wwdtm/__init__.py
@@ -25,4 +25,4 @@ from wwdtm.scorekeeper import (Scorekeeper,
                                ScorekeeperAppearances,
                                ScorekeeperUtility)
 
-VERSION = "2.0.0-alpha.7+exp.6"
+VERSION = "2.0.0-alpha.7+exp.7"
