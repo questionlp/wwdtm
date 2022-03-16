05d936f6045f1d70b7177f36b359ecf78c784357
Linh Pham <question+github@closedsrc.org>
2022-03-16 14:10:34 -0700

    Simplify logic around panelist and guest appearance scoring causing None to be passed through instead of zero

--- wwdtm/__init__.py
+++ wwdtm/__init__.py
@@ -21,4 +21,4 @@ from wwdtm.scorekeeper import Scorekeeper, ScorekeeperAppearances, ScorekeeperUt
 from wwdtm.show import Show, ShowInfo, ShowInfoMultiple, ShowUtility
 
 
-VERSION = "2.0.2"
+VERSION = "2.0.3"
