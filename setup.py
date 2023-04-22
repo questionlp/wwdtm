6ae756f61ae6379b5383b11efd23b6bea68b5e94
Linh Pham <questionlp@users.noreply.github.com>
2023-04-22 15:25:38 -0700

    Consolidate project info into pyproject.toml

--- setup.py
+++ setup.py
@@ -3,17 +3,8 @@
 #
 # Copyright (c) 2018-2023 Linh Pham
 # wwdtm is released under the terms of the Apache License 2.0
-"""Package setup file"""
+"""Deprecated: Package setup file"""
 
 from setuptools import setup
 
-setup(
-    name="wwdtm",
-    install_requires=[
-        "mysql-connector-python==8.0.33",
-        "numpy==1.24.2",
-        "python-dateutil==2.8.2",
-        "python-slugify==8.0.1",
-        "pytz==2023.3",
-    ],
-)
+setup()
