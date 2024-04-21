06ada74d28b20ff1015d387192583e4f7114c09e
Linh Pham <questionlp@users.noreply.github.com>
2024-04-20 20:44:57 -0700

    Bump version from beta to GA

--- wwdtm/__init__.py
+++ wwdtm/__init__.py
@@ -20,4 +20,4 @@ from wwdtm.panelist import (
 from wwdtm.scorekeeper import Scorekeeper, ScorekeeperAppearances, ScorekeeperUtility
 from wwdtm.show import Show, ShowInfo, ShowInfoMultiple, ShowUtility
 
-VERSION = "2.9.0-beta"
+VERSION = "2.9.0"
