e6c4abdbf4cf22acf401073cc26fe58ae2306a96
Linh Pham <questionlp@users.noreply.github.com>
2024-05-12 15:19:06 -0700

    Update version to 2.10.0

--- wwdtm/__init__.py
+++ wwdtm/__init__.py
@@ -25,7 +25,7 @@ from wwdtm.panelist import (
 from wwdtm.scorekeeper import Scorekeeper, ScorekeeperAppearances, ScorekeeperUtility
 from wwdtm.show import Show, ShowInfo, ShowInfoMultiple, ShowUtility
 
-VERSION = "2.10.0-beta.1"
+VERSION = "2.10.0"
 
 
 def database_version(
